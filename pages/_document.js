e8b6c1cb41bffba721e3a8fcd9c97b26483db9b5
Luke <l.ukefarrellyspain@.com>
2024-07-27 21:10:46 +0200

    PWA additions

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <meta name="theme-color" content="#69decf" />
+      </Head>
       <body className="bg-stone-50">
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -85,11 +85,7 @@ export default function Home() {
         <>
             <Head>
             <title>Genki Today!</title>
-            <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-            <link rel="shortcut icon" href="/icons/favicon.ico" />
-            <meta name="theme-color" content="#69decf" />
+            <meta name="viewport" content="initial-scale=1, viewport-fit=cover, width=device-width"></meta>
             </Head>
             <div className='flex fixed left-0 top-0 items-center justify-center w-screen h-20 backdrop-blur-md bg-teal-400/70 z-50'>
                 <div className='text-5xl p-4 text-white font-yellow'>Genki Today!</div>
