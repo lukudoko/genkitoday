1ed752edf0ff210338696902f6051c5a85dfdac8
Luke <58055712+lukudoko@users.noreply.github.com>
2024-08-08 20:55:34 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="robots" content="noindex" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" sizes="180x180" href="/splash/icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
